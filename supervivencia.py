10ac525cfbff48685f9b5ed7c2fc7d68754c859f
gudinos106-ui <gudinos106@gmail.com>
2026-05-11 12:23:05 -0600

    Update supervivencia.py

--- supervivencia.py
+++ supervivencia.py
@@ -812,7 +812,7 @@ else:
                             border-left: 8px solid #1f77b4; 
                             margin-bottom: 5px;
                             box-shadow: 2px 2px 5px rgba(0,0,0,0.1);">
-                    <small style="color: #555;">{row['FECHA']}</small><br>
+                    <small style="color: #555;">{row.get('FECHA', row.get('FECHA_REGISTRO', 'Sin fecha'))}
                     <strong style="font-size: 24px; color: #1f77b4;">👤 {row['AUTOR']} dice:</strong><br>
                     <p style="font-size: 22px; margin-top: 10px;">{row['MENSAJE']}</p>
                 </div>
